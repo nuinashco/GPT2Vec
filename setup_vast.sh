bc3b237aabcfa930ca082972d5cc4bd61c3a24c4
nuinashco <ivan.havlytskyi@gmail.com>
2025-05-11 17:38:45 +0300

    add setup

---
+++ setup_vast.sh
@@ -0,0 +1,16 @@
+# git
+# git clone https://nuinashco:$GITHUB_TOKEN@github.com/nuinashco/GPT2Vec.git
+# cd GPT2Vec
+
+git config --global user.name "Ivan Havlytskyi"
+git config --global user.email "ivan.havlytskyi@gmail.com"
+
+# poetry
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="/root/.local/bin:$PATH"
+poetry config virtualenvs.in-project true
+poetry install
+
+# login
+huggingface-cli login --token $HUGGINGFACE_TOKEN
+wandb login

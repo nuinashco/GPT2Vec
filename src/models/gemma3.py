ebd2358c183d6446c2f34feb0750cd8c8dcbd161
Ivan Havlytskyi <ivan.havlytskyi@gmail.com>
2025-06-02 20:25:12 +0000

    add sl gemma

--- src/models/gemma3.py
+++ src/models/gemma3.py
@@ -369,6 +369,7 @@ class biGemma3ForTokenClassification(Gemma3PreTrainedModel):
         use_cache: Optional[bool] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
+        **kwargs
     ) -> TokenClassifierOutput:
         r"""
         labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
